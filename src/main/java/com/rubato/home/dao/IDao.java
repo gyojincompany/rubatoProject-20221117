d6732aa6030100e413145c773d4ae92b03abe34f
gyojincompany <luckyred@gmail.com>
2022-11-17 15:29:32 +0900

    로그인 폼 변경 완료

--- src/main/java/com/rubato/home/dao/IDao.java
+++ src/main/java/com/rubato/home/dao/IDao.java
@@ -3,5 +3,6 @@ package com.rubato.home.dao;
 public interface IDao {
 	
 	public void joinMember(String mid, String mpw, String mname, String memail);//insert
+	public int checkUserId(String mid);//select
 	
 }
